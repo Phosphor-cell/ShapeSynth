a2ccd22ff2f687e0aa751e3da3498a4ba2259e61
Phosphor-cell <devbhaven@gmail.com>
2025-05-28 20:06:14 -0600

    updated and working Bounding Box

--- src/retopo.py
+++ src/retopo.py
@@ -2,15 +2,14 @@ import os
 import math
 import matplotlib.pyplot as ply
 import numpy as np
-import pandas as pd
+#! import pandas as pd
 
 
-"""
-    Software Based Retopology, Product of ShapeSynth
-    Author: Phosphor-cell(Dev Bhatt)
-    Version: 0.0.0A
-    Date:2025-05-26
-"""
+#* Software Based Retopology, Product of ShapeSynth
+#* Author: Phosphor-cell(Dev Bhatt)
+#* Version: 0.0.0A
+#* Date:2025-05-26
+
 
 VERTEXES = []
 FACES = []
@@ -27,8 +26,10 @@ def extract_shape(filepath: str):
                 line = np.float32(np.array(line))
                 VERTEXES.append(line[:3])
             
-            if "elbow" in lines and not "off" in lines:
+            if "elbow" in lines and not "off" in lines or 'g elbow' in lines:
                 groupOn = True
+            elif "g off" in lines:
+                groupOn = False
             if groupOn and "f " in lines:
                 face_lines = lines.strip("f").strip("\n").split("//")[0]
                 face_lines = face_lines.strip(" ")
@@ -37,19 +38,30 @@ def extract_shape(filepath: str):
     temp_groups = set(FACES)
     FACES = np.array(list(temp_groups), dtype=np.int64)
 
-def vertex_setter(vertstoFaces):
-    #VIew Points = [Highest, Lowest, Right Most, Left Most, Furthest, Closest]
-    view_points = []
-    repoints = []
-    for indexes in FACES:
-        for i in range (0,3):
-            for j in range(0,3):
-                if not len(view_points) < i or VERTEXES[indexes-1][i] > view_points[i][j]:
-                    view_points.insert(i, VERTEXES[indexes-1])
-                elif not len(view_points) < (4+(i%3)) or VERTEXES[indexes-1][i] < view_points[4+(i%3)][j]:
-                    view_points.insert(4+(i%3), VERTEXES[indexes-1])
-    print(view_points)
 
-extract_shape("mesh_1.obj")
+def vertex_setter(density: int):
+    #bounding_indexes = [closest to center, max cords, min cords,]
+    center_point = []
+    bounding_box= []
+    shrink_wrap = []
+    bounding_indexes = []
+    for i in FACES:
+        bounding_box.append(VERTEXES[i-1])
+
+    bounding_box = np.array(bounding_box)
+    center_point = bounding_box.mean(axis=0)
+    bounding_indexes.extend([
+        np.sum((bounding_box-center_point)**2, axis=1, keepdims=True).argmin(axis=0), 
+        bounding_box.argmax(axis=0),
+        bounding_box.argmin(axis=0),
+        ]
+    )
+    
+    for fst_index in range(len(bounding_indexes)):
+        for i in range(len(bounding_indexes[fst_index])):
+            shrink_wrap.append(bounding_indexes[fst_index][i])
+    
+
+extract_shape("mesh_7.obj")
 print("Done")
-vertex_setter(FACES)
+vertex_setter(6)
